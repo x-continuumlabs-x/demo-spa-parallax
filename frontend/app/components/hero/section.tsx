79299b5f49460578cb65e2a71ff773ef64ab4109
x-continuumlabs.io <continuumlabsio@proton.me>
2025-10-13 21:14:22 +0200

    Add basic hero image fade in animation

--- frontend/app/components/hero/section.tsx
+++ frontend/app/components/hero/section.tsx
@@ -1,13 +1,28 @@
+"use client";
+
 import Image from "next/image";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Hero() {
 	const fullWidth = 3200;
 	const fullHeight = 2196;
 	const heightRatio = fullHeight / fullWidth;
+	const imageContainerRef = useRef<HTMLDivElement>(null);
+
+	useGSAP(() => {
+		gsap.fromTo(
+			imageContainerRef.current,
+			{ width: "110vw" },
+			{ width: "100vw", duration: 3.5, ease: "power4.out" }
+		);
+	});
+
 	return (
 		<section className="relative w-full overflow-hidden">
 			<div className="relative w-full" style={{ height: `calc(${heightRatio} * 100vw)` }}>
-				<div className="absolute top-0 left-1/2 -translate-x-1/2 w-[100vw] h-full">
+				<div ref={imageContainerRef} className="absolute top-0 left-1/2 -translate-x-1/2 w-[100vw] h-full">
 					<Image
 						src="/local-images/section-bg-hero.jpg"
 						alt="Photo portrait of a young woman"
