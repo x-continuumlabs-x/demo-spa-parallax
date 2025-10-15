f8a3864342a69936475f4412f857dc764e821ee0
x-continuumlabs.io <continuumlabsio@proton.me>
2025-10-15 22:51:59 +0200

    Add about section placeholder copy

--- frontend/app/components/about/section.tsx
+++ frontend/app/components/about/section.tsx
@@ -1,7 +1,25 @@
 export default function About(){
 	return(
 		<section className="relative w-full overflow-hidden">
-			<h1>ABOUT Ut enim ad minim veniam</h1>
+			<div className="flex justify-evenly">
+				<div className="w-1/3">
+					<div>
+						<h3 className="text-[22px] uppercase font-nominee font-black tracking-[-0.03em] leading-[0.8em]">2016</h3>
+						<p>Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</p>
+					</div>
+				</div>
+				<div className="w-1/3">
+					<h1 className="text-[5vw] uppercase font-nominee font-black tracking-[-0.08em] leading-[0.8em] mb-[15px]">Ut enim <br />ad minim <br />veniam</h1>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				
+					<h2 className="text-[2vw] font-nominee font-medium tracking-[-0.03em] leading-[1.0em] mb-[15px]">Ullamco Laboris</h2>
+
+					<p>Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
 		</section>
 	);
 }
--- frontend/app/components/services/section.tsx
+++ frontend/app/components/services/section.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 export default function Services(){
-	const fullWidth = 2235;
+	const fullWidth = 2665;
 	const fullHeight = 1468;
 	return(
 		<section className="top-[-30vh] relative w-full overflow-hidden flex justify-end" >
-			<div className="relative">
+			<div className="relative mr-[-5vw]">
 				<Image
-					src="/local-images/section-bg-services.jpg"
+					src="/local-images/section-bg-services-desktop.jpg"
 					alt="Photo portrait of an old man"
 					width={fullWidth}
 					height={fullHeight}
@@ -19,7 +19,7 @@ export default function Services(){
 					}}
 					priority
 				/>
-				<div className="absolute top-[25vh] left-[-5vw]" data-speed="0.9">
+				<div className="absolute top-[25vh] left-[-9vw]" data-speed="0.9">
 					<h1 className="text-[3vw] uppercase font-nominee font-black tracking-[-0.08em] leading-[1.0em] mb-[15px]">$599 Ut <br />architecto <br />voluptatem</h1>
 					<p className="w-[25vw] max-w-[410px] text-[15px] leading-[1.0em] opacity-60">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
 				</div>
