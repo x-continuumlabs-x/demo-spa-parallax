e0898d811707c5b61b04200771c706181b8f0a57
x-continuumlabs.io <continuumlabsio@proton.me>
2025-11-30 15:53:01 +0100

    Set up basic scroll trigger sequence animation.

--- frontend/app/components/highlights/section.tsx
+++ frontend/app/components/highlights/section.tsx
@@ -1,9 +1,74 @@
 import { Props } from "@/types";
+import { useRef } from "react";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
 export default function Highlights({ wrapperRef }: Props) {
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+
+	useGSAP(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+		const context = canvas.getContext("2d");
+		if (!context) return;
+
+		const width = 1280;
+		const height = 720;
+		canvas.width = width;
+		canvas.height = height;
+
+		// --- Image Sequence Setup ---
+		const startFrame = 36;
+		const endFrame = 192;
+		const frameCount = endFrame - startFrame + 1;
+
+		const images: HTMLImageElement[] = [];
+		const state = { frame: 0 };
+
+		const currentFrame = (index: number) =>
+		`/local-images/frames/frame_${String(index).padStart(5, "0")}.jpg`;
+
+		// Preload frames
+		for (let i = startFrame; i <= endFrame; i++) {
+			const img = new window.Image();
+			img.src = currentFrame(i);
+			images.push(img);
+		}
+
+		const render = () => {
+		context.clearRect(0, 0, width, height);
+		const img = images[state.frame];
+		if (img) {
+			context.drawImage(img, 0, 0, width, height);
+		}
+		};
+
+		images[0].onload = render;
+
+		// --- GSAP Scroll Animation ---
+		gsap.to(state, {
+		frame: frameCount - 1,
+		snap: "frame",
+		ease: "none",
+		scrollTrigger: {
+			trigger: canvas,
+			start: "bottom bottom",
+			end: "bottom+=2000 top",
+			scrub: 0.5,
+		},
+		onUpdate: render,
+		});
+
+	}, { scope: wrapperRef });
 	return(
 		<section className="relative w-full overflow-hidden">
+			<div className="w-full flex justify-center">
+				<canvas ref={canvasRef} />
+			</div>
 			<div data-speed="1.2">
 				<Image
 					src="/local-images/section-bg-highlights.jpg"
