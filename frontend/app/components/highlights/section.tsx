ffd1c50871e760bb34e1fa74c44ff0f901c9f3ad
x-continuumlabs.io <continuumlabsio@proton.me>
2025-12-08 20:06:12 +0100

    Break up Highlights section H1 into individual SVG's to animated as a masked sequence.

--- frontend/app/components/highlights/section.tsx
+++ frontend/app/components/highlights/section.tsx
@@ -80,8 +80,55 @@ export default function Highlights({ wrapperRef }: Props) {
 				<canvas ref={canvasRef} />
 			</div>
 			<div className="absolute top-0 left-0 w-full h-[100vh] flex flex-col items-center justify-center">
+				<div className="w-[98vw] aspect-[1299/351] relative flex flex-row">
+					<div className="w-[17.32%] h-[100%] absolute">
+						<img
+							src="/headings/highlights-h1-v1.svg"
+						/>
+					</div>
+					<div className="w-[13.7%] absolute left-[13.63%]">
+						<img
+							src="/headings/highlights-h1-v2.svg"
+						/>
+					</div>
+					<div className="w-[19.25%] absolute left-[27.68%]">
+						<img
+							src="/headings/highlights-h1-e1.svg"
+						/>
+					</div>
+					<div className="w-[11.7%] absolute left-[35.22%]">
+						<img
+							src="/headings/highlights-h1-e2.svg"
+						/>
+					</div>
+					<div className="w-[9.43%] absolute top-[38.37%] left-[35.22%]">
+						<img
+							src="/headings/highlights-h1-e3.svg"
+						/>
+					</div>
+					<div className="w-[19.25%] absolute left-[48.5%]">
+						<img
+							src="/headings/highlights-h1-l1.svg"
+						/>
+					</div>
+					<div className="w-[7.58%] absolute left-[68.59%]">
+						<img
+							src="/headings/highlights-h1-i1.svg"
+						/>
+					</div>
+					<div className="w-[7.62%] absolute top-[24.3%] left-[84.95%]">
+						<img
+							src="/headings/highlights-h1-t1.svg"
+						/>
+					</div>
+					<div className="w-[22.56%] absolute left-[77.44%]">
+						<img
+							src="/headings/highlights-h1-t2.svg"
+						/>
+					</div>
+				</div>
 				<div className="text-center flex flex-col items-center">
-					<h1 className="text-[36.0vw] text-[#b0f3ee] uppercase font-nominee font-black tracking-[-0.08em] leading-[0.9em] m-0 ml-[-0.08em]">Velit</h1>
+					{/* <h1 className="text-[36.0vw] text-[#b0f3ee] uppercase font-nominee font-black tracking-[-0.08em] leading-[0.9em] m-0 mt-[-5vh] ml-[-0.08em]">Velit</h1> */}
 					<p className="w-1/2 text-[18px] text-[#b0f3ee] leading-[1.2em]">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ipsum dolor sit amet, consec.
 					</p>
