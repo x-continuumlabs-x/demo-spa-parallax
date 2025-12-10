a203f977e33bab2fe1015628adfed1a6886b8fea
x-continuumlabs.io <continuumlabsio@proton.me>
2025-12-10 16:00:42 +0000

    Temporarily hide hero image sequence.

--- frontend/app/components/highlights/section.tsx
+++ frontend/app/components/highlights/section.tsx
@@ -186,7 +186,7 @@ export default function Highlights({ wrapperRef }: Props) {
 	}, { scope: wrapperRef });
 	return(
 		<section className="relative w-full overflow-hidden">
-			<div className="w-full h-[100vh] flex justify-center">
+			<div className="w-full h-[100vh] flex justify-center opacity-0">
 				<canvas ref={canvasRef} />
 			</div>
 			<div className="absolute top-0 left-0 w-full h-[100vh] flex flex-col items-center justify-center">
