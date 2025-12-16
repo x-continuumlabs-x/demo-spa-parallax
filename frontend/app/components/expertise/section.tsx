56e6dc364f1741c9e5282e442396cf60dfb1ba4b
x-continuumlabs.io <continuumlabsio@proton.me>
2025-12-16 10:21:05 -0800

    Set portrait/landscape logic for cards pinning.

--- frontend/app/components/expertise/section.tsx
+++ frontend/app/components/expertise/section.tsx
@@ -23,10 +23,12 @@ export default function Expertise({ wrapperRef }: Props){
 	const heightRatioPortrait = imgHeightPortrait / imgWidthPortrait;
 
 	useGSAP( () => {
+		const startPosition = window.innerWidth < 640 ? 'top 40%' : 'center center';
+
 		ScrollTrigger.create({
 			trigger: '#cardsID',
 			pin: true,
-			start: 'center center',
+			start: startPosition,
 			end: '+=450',
 		});
 		},
@@ -91,7 +93,7 @@ export default function Expertise({ wrapperRef }: Props){
 				<h1 className="text-[35vw] sm:text-[30vw] text-[#b8bc92] uppercase font-nominee font-black tracking-[-0.08em] leading-[0.8em] text-center m-0 ml-[-0.08em]">Amet</h1>
 			</div>
 
-			<div className="absolute top-[30vw] sm:top-[18vw] sm:left-[10vw] px-[8%] sm:px-0 sm:w-[28%] text-center sm:text-left text-[#b8bc92]" data-speed={isMobile ? "0.8" : "0.6"}>
+			<div className="absolute top-[30vw] sm:top-[25vw] sm:left-[10vw] px-[8%] sm:px-0 sm:w-[28%] text-center sm:text-left text-[#b8bc92]" data-speed={isMobile ? "0.8" : "0.6"}>
 				<h3 className="font-nominee font-black text-[18px] tracking-[-0.06em] uppercase">Magna Aliqua</h3>
 				<p className="text-[18px] leading-[1.2em]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
 			</div>
