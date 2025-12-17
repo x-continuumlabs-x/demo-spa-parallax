a9b4e8f660f03f76597019d7c158443f18971c6e
x-continuumlabs.io <continuumlabsio@proton.me>
2025-12-16 17:13:43 -0800

    Update footer mobile styles.

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer(){
 	return(
 		<div className="p-[20px] ">
-			<div className="flex justify-between w-full p-10 bg-[#1f1c1b]">
+			<div className="flex justify-between w-full p-5 sm:p-10 bg-[#1f1c1b]">
 				<div>
-					<img className="w-[200px]" src="/logo-2.svg" />
+					<img className="w-[160px] sm:w-[200px]" src="/logo-2.svg" />
 				</div>
 				<div>
-					<p className="text-[12px] opacity-40">&copy; Copyright 2026</p>
+					<p className="text-[12px] opacity-40 text-right">&copy; Copyright 2026</p>
 				</div>
 			</div>
 		</div>
