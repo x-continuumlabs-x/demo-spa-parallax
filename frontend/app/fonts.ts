bb90e1c19b35186d8b46446b8c1a072873b20464
x-continuumlabs.io <continuumlabsio@proton.me>
2025-12-18 19:43:43 -0800

    Replace fonts to fix glyphs issues.

--- frontend/app/fonts.ts
+++ frontend/app/fonts.ts
@@ -2,24 +2,24 @@ import localFont from "next/font/local";
 import { Albert_Sans } from "next/font/google";
 
 export const mainfont = localFont({
-  src: [
-    {
-      path: "../public/fonts/Mainfont-Medium.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Mainfont-Black.otf",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-mainfont",
-  weight: "500",
+	src: [
+	{
+		path: "../public/fonts/MainfontMedium.otf",
+		weight: "500",
+		style: "normal",
+	},
+	{
+		path: "../public/fonts/MainfontBlack.otf",
+		weight: "900",
+		style: "normal",
+	},
+	],
+	variable: "--font-mainfont",
+	weight: "500",
 });
 
 export const albertSans = Albert_Sans({
-  subsets: ["latin"],
-  weight: ["500", "700"],
-  variable: "--font-albert-sans",
+	subsets: ["latin"],
+	weight: ["500", "700"],
+	variable: "--font-albert-sans",
 });
